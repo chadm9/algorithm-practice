6b8e9b18e787e65289b5d5e3c1019c292a9ba8f0
Chad McKee <chadm_@yahoo.com>
2017-08-30 22:14:38 -0400

    bogo sort

--- sorting-algorithms.py
+++ sorting-algorithms.py
@@ -151,3 +151,34 @@ def heap_sort():
 sqc = [2, 7, 1, -2, 56, 5, 3]
 heap_sort()
 print(sqc)
+
+
+#bogosort
+
+from random import *
+from time import *
+
+#seed()
+x = []
+for i in range(0, 10):
+    x.append(randint(0, 100))
+
+def inorder(x):
+    i = 0
+    j = len(x)
+    while i + 1 < j:
+        if x[i] > x[i + 1]:
+            return False
+        i += 1
+    return True
+
+def bogo(x):
+    while not inorder(x):
+        shuffle(x)
+    return x
+
+start = time()
+print "Before: ", x
+x = bogo(x)
+print "After: ", x
+print "%.2f seconds" % (time() - start)
